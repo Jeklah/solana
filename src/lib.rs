cd8ee015b366396616e0bc2282c069211bd90645
Jeklah <arthurbowers@gmail.com>
2021-05-02 15:08:23 +0100

    changed lib to add pub mod entryfile

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,2 @@
-use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
-};
-
-entrypoint!(process_instruction);
-fn process_instruction(
-    program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    instruction_data: &[u8],
-) -> ProgramResult {
-    msg!(
-        "process_instruction: {}: {} accounts, data={:?}",
-        program_id,
-        accounts.len(),
-        instruction_data
-    );
-    Ok(())
-}
-
+// inside lib.rs, onlythe following line should be here
+pub mod entrypoint;
