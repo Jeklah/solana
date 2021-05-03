1cb1993cdffe0b919d71e8a3a4bbe352526b30ce
Jeklah <arthurbowers@gmail.com>
2021-05-03 17:21:08 +0100

    Correcting use account_info statement.

--- src/processor.rs
+++ src/processor.rs
@@ -1,5 +1,5 @@
 use solana_program::{
-    account_info::AccountInfo,
+    account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
     program_error::ProgramError,
     msg,
