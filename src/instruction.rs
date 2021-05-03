c032dadf1c76bfc663c6971918b0c42500ae9337
Jeklah <arthurbowers@gmail.com>
2021-05-03 16:39:40 +0100

    Adding instruction.rs file.

---
+++ src/instruction.rs
@@ -0,0 +1,24 @@
+// inside instruction.rs
+
+pub enum EscrowInstruction {
+
+    /// Starts the trade by creating and populating an escrow account and transferring ownership
+    /// of the given temp token account to the PDA
+    ///
+    ///
+    ///
+    /// Accounts expected:
+    ///
+    /// 0. `[signer]` The account of the person initializing the escrow
+    /// 1. `[writable]` Temporary token account that should be created prior to this instruction
+    ///     and owned by the initializer.
+    /// 2; `[]` The initializer's token account for the token they will receive should the trade go
+    ///     through.
+    /// 3. `[writable]` The escrow account, it will hold all necessary info about the trade.
+    /// 4. `[]` The rent sysvar
+    /// 5. `[]` The token program
+    InitEscrow {
+        /// The amount part A expects to receive of token Y
+        amount: u64
+    }
+}
