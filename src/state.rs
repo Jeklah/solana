79015ac86f55872a6b957be616b93322c03b38a2
Jeklah <arthurbowers@gmail.com>
2021-05-03 21:48:09 +0100

    Adding impl Pack to state.rs

--- src/state.rs
+++ src/state.rs
@@ -1,4 +1,25 @@
-use solana_program::pubkey::Pubkey
+use solana_program::{
+    program_pack::{IsInitialized, Pack, Sealed},
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
+use arrayref::{array_mut_ref, array_ref, array_refs, mut_arrary_refs};
+
+impl Sealed for Escrow {}
+
+impl IsInitialized for Escrow {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
+}
+
+impl Pack for Escrow {
+    const LEN: usize = 105;
+    fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
+
+    }
+}
 
 pub struct Escrow {
     pub is_initialized: bool,
