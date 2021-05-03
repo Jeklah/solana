c9a6476d5a5bd25ebff8251c75b0c732f72a6805
Jeklah <arthurbowers@gmail.com>
2021-05-03 17:12:56 +0100

    Adding Processor struc to processor.rs

---
+++ src/processor.rs
@@ -0,0 +1,22 @@
+use solana_program::{
+    account_info::AccountInfo,
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+};
+
+use crate::instruction::EscrowInstruction;
+
+pub struct Processor;
+impl Processor {
+    pub fn process(program_id: &Pubkeu, accounts: &[AccountInfo], instruction_data: &[u8]) -> ProgramResult {
+        let instruction = EscrowInstruction::unpack(instruction_data)?;
+
+        match instruction {
+            EscrowInstruction::InitEscrow { amount } => {
+                msg!("Instruction: InitEscrow");
+                Self::process_init_escrow(accounts, amount, program_id)
+            }
+        }
+    }
+}
