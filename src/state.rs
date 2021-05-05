758e72625b464c2adbb49f9acaea325ce41b78b0
Jeklah <arthurbowers@gmail.com>
2021-05-05 20:52:17 +0100

    Started implementation of state.rs

--- src/state.rs
+++ src/state.rs
@@ -17,7 +17,30 @@ impl IsInitialized for Escrow {
 impl Pack for Escrow {
     const LEN: usize = 105;
     fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
+        const LEN: usize = 105;
+        fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
+            let src = array_ref![src, 0, Escrow::LEN];
+            let (
+                is_initialized,
+                initializer_pubkey,
+                temp_token_account_pubkey,
+                initializer_token_to_receive_account_pubkey,
+                expected_amount,
+            ) = array_refs![src, 1, 32, 32, 32, 8];
+            let is_initialized = match is_initialized {
+                [0] => false,
+                [1] => true,
+                _ => return Err(ProgramError::InvalidAccountData),
+            };
 
+            Ok(Escrow {
+                is_initialized,
+                initializer_pubkey: Pubkey::new_from_array(*initializer_pubkey),
+                temp_token_account_pubkey: Pubkey::new_from_array(*temp_token_account_pubkey),
+                initializer_token_to_receive_account_pubkey: Pubkey::new_from_array(*initializer_token_to_receive_account_pubkey),
+                expected_amount: u64::from_le_bytes(*expected_amount),
+            })
+        }
     }
 }
 
