c2ee83ca98ab43d16dca2522075d71b5e81b684a
Jeklah <arthurbowers@gmail.com>
2021-05-21 17:08:01 +0100

    Corrected use crate to use instruction not error.. Changed layout of fn process. Missing semi colons. Enclosed Escrow statement in (). Changed _bump_seed to _nonce.

--- src/processor.rs
+++ src/processor.rs
@@ -9,10 +9,14 @@ use solana_program::{
     program::invoke
 };
 
-use crate::{instruction::EscrowInstruction, error::EscrowError, state::Escrow};
+use crate::{error::EscrowError, instruction::EscrowInstruction, state::Escrow};
+
 pub struct Processor;
 impl Processor {
-    pub fn process(program_id: &Pubkeu, accounts: &[AccountInfo], instruction_data: &[u8]) -> ProgramResult {
+    pub fn process(program_id: &Pubkey,
+                   accounts: &[AccountInfo],
+                   instruction_data: &[u8],
+                ) -> ProgramResult {
         let instruction = EscrowInstruction::unpack(instruction_data)?;
 
         match instruction {
@@ -29,7 +33,7 @@ impl Processor {
         program_id: &Pubkey,
     ) -> ProgramResult {
         let account_info_iter = &mut accounts.item();
-        let initializer = next_account_info(account_info_item)?;
+        let initializer = next_account_info(account_info_iter)?;
 
         if !initializer.is_signer {
             return Err(ProgramError::MissingRequiredSignature);
@@ -55,13 +59,13 @@ impl Processor {
         }
 
         escrow_info.is_initialized = true;
-        escrow_info.initializer_pubkey = *initializer_key;
+        escrow_info.initializer_pubkey = *initializer.key;
         escrow_info.temp_token_account_pubkey = *temp_token_account.key;
         escrow_info.initializer_token_to_receive_account_pubkey = *token_to_receive_account.key;
         escrow_info.expected_amount = amount;
 
-        Escrow:pack(escrow_info, &mut escrow_account.data.borrow_mut())?;
-        let (pds, _bump_seed) = PubKey::find_program_address(&[b"escrow"], program_id);
+        (Escrow::pack(escrow_info, &mut escrow_account.data.borrow_mut()))?;
+        let (pda, _nonce) = Pubkey::find_program_address(&[b"escrow"], program_id);
         let token_program = next_account_info(account_info_iter)?;
         let owner_change_ix = spl_token::instruction::set_authority(
             token_program.key,
