3402833afa18c70a74a57f6fa1d13b55be55dd63
Jeklah <arthurbowers@gmail.com>
2021-05-21 17:12:44 +0100

    Mispelling of array and alignment + change {} 2 ().

--- src/state.rs
+++ src/state.rs
@@ -4,7 +4,7 @@ use solana_program::{
     pubkey::Pubkey,
 };
 
-use arrayref::{array_mut_ref, array_ref, array_refs, mut_arrary_refs};
+use arrayref::{array_mut_ref, array_ref, array_refs, mut_array_refs};
 
 impl Sealed for Escrow {}
 
@@ -17,41 +17,38 @@ impl IsInitialized for Escrow {
 impl Pack for Escrow {
     const LEN: usize = 105;
     fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
-        const LEN: usize = 105;
-        fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
-            let src = array_ref![src, 0, Escrow::LEN];
-            let (
-                is_initialized,
-                initializer_pubkey,
-                temp_token_account_pubkey,
-                initializer_token_to_receive_account_pubkey,
-                expected_amount,
+        let src = array_ref![src, 0, Escrow::LEN];
+        let (
+            is_initialized,
+            initializer_pubkey,
+            temp_token_account_pubkey,
+            initializer_token_to_receive_account_pubkey,
+            expected_amount,
             ) = array_refs![src, 1, 32, 32, 32, 8];
-            let is_initialized = match is_initialized {
-                [0] => false,
-                [1] => true,
-                _ => return Err(ProgramError::InvalidAccountData),
-            };
+        let is_initialized = match is_initialized {
+            [0] => false,
+            [1] => true,
+              _ => return Err(ProgramError::InvalidAccountData),
+        };
 
-            Ok(Escrow {
-                is_initialized,
-                initializer_pubkey: Pubkey::new_from_array(*initializer_pubkey),
-                temp_token_account_pubkey: Pubkey::new_from_array(*temp_token_account_pubkey),
-                initializer_token_to_receive_account_pubkey: Pubkey::new_from_array(*initializer_token_to_receive_account_pubkey),
-                expected_amount: u64::from_le_bytes(*expected_amount),
-            })
-        }
+        Ok(Escrow {
+        is_initialized,
+        initializer_pubkey: Pubkey::new_from_array(*initializer_pubkey),
+        temp_token_account_pubkey: Pubkey::new_from_array(*temp_token_account_pubkey),
+        initializer_token_to_receive_account_pubkey: Pubkey::new_from_array(*initializer_token_to_receive_account_pubkey),
+        expected_amount: u64::from_le_bytes(*expected_amount),
+        })
     }
 
     fn pack_into_slice(&self, dst: &mut [u8]) {
         let dst = array_mut_ref![dst, 0, Escrow::LEN];
-        let {
+        let (
             is_initialized_dst,
             initializer_pubkey_dst,
             temp_token_account_pubkey_dst,
             initializer_token_to_receive_account_pubkey_dst,
             expected_amount_dst,
-        } = mut_arrary_refs![dst, 1, 32, 32, 32, 8];
+        ) = mut_array_refs![dst, 1, 32, 32, 32, 8];
 
         let Escrow {
             is_initialized,
